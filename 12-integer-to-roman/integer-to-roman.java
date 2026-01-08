0007b1fb9a8348747a4a2d8e2b4f17fc47bfa7a7
jeeva <jeevapriyan27116@gmail.com>
2026-01-08 19:31:41 +0530

    Time: 7 ms (15.21%) | Memory: 46.9 MB (10.42%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String intToRoman(int num) {
+        String thousand[] = {"","M","MM","MMM",};
+        String hundred[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX","X"};
+        return( thousand[num/1000] +
+                hundred[(num%1000)/100]+
+                tens[(num%100)/10]+
+                ones[(num%10)/1]);
+    }
+}
