e6a98860d4b5b287168735c0f0ccd9694d459cee
jeeva <jeevapriyan27116@gmail.com>
2026-05-06 11:48:47 +0530

    Time: 4 ms (80.29%) | Memory: 47.1 MB (14.50%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,39 +10,66 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
-            return null;
-        }
+    //     if (lists == null || lists.length == 0) {
+    //         return null;
+    //     }
 
-        while (lists.length > 1) {
-            List<ListNode> temp = new ArrayList<>();
-            for (int i = 0; i < lists.length; i += 2) {
-                ListNode l1 = lists[i];
-                ListNode l2 = i + 1 < lists.length ? lists[i + 1] : null;
-                temp.add(mergeLists(l1, l2));
+    //     while (lists.length > 1) {
+    //         List<ListNode> temp = new ArrayList<>();
+    //         for (int i = 0; i < lists.length; i += 2) {
+    //             ListNode l1 = lists[i];
+    //             ListNode l2 = i + 1 < lists.length ? lists[i + 1] : null;
+    //             temp.add(mergeLists(l1, l2));
+    //         }
+    //         lists = temp.toArray(new ListNode[0]);
+    //     }
+
+    //     return lists[0];        
+    // }
+
+    // private ListNode mergeLists(ListNode l1, ListNode l2) {
+    //     ListNode node = new ListNode();
+    //     ListNode ans = node;
+
+    //     while (l1 != null && l2 != null) {
+    //         if (l1.val > l2.val) {
+    //             node.next = l2;
+    //             l2 = l2.next;
+    //         } else {
+    //             node.next = l1;
+    //             l1 = l1.next;
+    //         }
+    //         node = node.next;
+    //     }
+
+    //     node.next = l1 != null ? l1 : l2;
+    //     return ans.next;
+     PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Add first node of every list
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(node);
             }
-            lists = temp.toArray(new ListNode[0]);
         }
 
-        return lists[0];        
-    }
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
 
-    private ListNode mergeLists(ListNode l1, ListNode l2) {
-        ListNode node = new ListNode();
-        ListNode ans = node;
+        while (!pq.isEmpty()) {
 
-        while (l1 != null && l2 != null) {
-            if (l1.val > l2.val) {
-                node.next = l2;
-                l2 = l2.next;
-            } else {
-                node.next = l1;
-                l1 = l1.next;
+            ListNode minNode = pq.poll();
+
+            tail.next = minNode;
+            tail = tail.next;
+
+            // Add next node
+            if (minNode.next != null) {
+                pq.add(minNode.next);
             }
-            node = node.next;
         }
 
-        node.next = l1 != null ? l1 : l2;
-        return ans.next;
+        return dummy.next;
     }
 }
